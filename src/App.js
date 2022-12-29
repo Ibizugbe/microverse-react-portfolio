3d97e054a6f5c12f85aa9764f3f3debbec85ecd5
Abraham Ibizugbe <abrahamibizugbe@gmail.com>
2022-12-29 18:43:10 +0100

    add min-width property to app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Contact from './components/Contact';
 import Intro from './components/Intro';
 import Navbar from './components/Navbar';
 
@@ -10,6 +11,7 @@ function App() {
       </header>
       <main>
         <Intro />
+        <Contact />
       </main>
     </>
   );
