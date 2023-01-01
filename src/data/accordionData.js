a11b53a3a7f0563771f7d8bf44bb6ccd509eab1c
Abraham Ibizugbe <abrahamibizugbe@gmail.com>
2023-01-01 22:48:18 +0100

    import images to accordion data

--- src/data/accordionData.js
+++ src/data/accordionData.js
@@ -1,13 +1,24 @@
+import jsImg from "../Assets/languages/javascript.png";
+import rubyImg from "../Assets/languages/ruby.png";
+import htmlImg from "../Assets/languages/html.png";
+import postgresImg from "../Assets/languages/postgres.png";
+import sqlImg from "../Assets/languages/sql.png";
+import gitImg from "../Assets/languages/github.png";
+import reactImg from "../Assets/languages/react.png";
+import railsImg from "../Assets/languages/railsL.png";
+import bootstrapImg from "../Assets/languages/bootstrap.png";
+import tailwindImg from "../Assets/languages/tailwind.png";
+
 const accordionData = [
   {
-    title: 'Languages',
+    title: "Languages",
     content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis sapiente
     laborum cupiditate possimus labore, hic temporibus velit dicta earum
     suscipit commodi eum enim atque at? Et perspiciatis dolore iure
     voluptatem.`,
   },
   {
-    title: 'Frameworks',
+    title: "Frameworks",
     content: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia veniam
     reprehenderit nam assumenda voluptatem ut. Ipsum eius dicta, officiis
     quaerat iure quos dolorum accusantium ducimus in illum vero commodi
@@ -16,13 +27,13 @@ const accordionData = [
     Repudiandae, mollitia id reprehenderit a ab odit!`,
   },
   {
-    title: 'Skills',
+    title: "Skills",
     content: `Sapiente expedita hic obcaecati, laboriosam similique omnis architecto ducimus magnam accusantium corrupti
     quam sint dolore pariatur perspiciatis, necessitatibus rem vel dignissimos
     dolor ut sequi minus iste? Quas?`,
   },
   {
-    title: 'Tools',
+    title: "Tools",
     content: `Sapiente expedita hic obcaecati, laboriosam similique omnis architecto ducimus magnam accusantium corrupti
     quam sint dolore pariatur perspiciatis, necessitatibus rem vel dignissimos
     dolor ut sequi minus iste? Quas?`,
