f0d5de4d14772121d48dbda56fcca1748cdc6b45
Abraham Ibizugbe <abrahamibizugbe@gmail.com>
2023-01-02 10:13:06 +0100

    fix linter errors

--- src/data/accordionData.js
+++ src/data/accordionData.js
@@ -1,30 +1,20 @@
-import jsImg from "../Assets/languages/javascript.png";
-import rubyImg from "../Assets/languages/ruby.png";
-import htmlImg from "../Assets/languages/html.png";
-import postgresImg from "../Assets/languages/postgres.png";
-import sqlImg from "../Assets/languages/sql.png";
-import gitImg from "../Assets/languages/github.png";
-import reactImg from "../Assets/languages/react.png";
-import railsImg from "../Assets/languages/railsL.png";
-import bootstrapImg from "../Assets/languages/bootstrap.png";
-import tailwindImg from "../Assets/languages/tailwind.png";
-
 const accordionData = [
   {
-    title: "Languages",
-    content: `Ruby, HTML, CSS and Javascript`,
+    title: 'Languages',
+    content: 'Ruby, HTML, CSS and Javascript',
   },
   {
-    title: "Frameworks",
-    content: `Bootstrap, Tailwind, React, Rails`,
+    title: 'Frameworks',
+    content: 'Bootstrap, Tailwind, React, Rails',
   },
   {
-    title: "Skills",
-    content: `Remote Pair-Programming, Teamwork, Mentoring`,
+    title: 'Skills',
+    content: 'Remote Pair-Programming, Teamwork, Mentoring',
   },
   {
-    title: "Tools",
-    content: `Git, GitHub, Heroku, Netlify, Mobile/Responsive Development, RSpec, TDD, Chrome Dev Tools `,
+    title: 'Tools',
+    content:
+      'Git, GitHub, Heroku, Netlify, Mobile/Responsive Development, RSpec, TDD, Chrome Dev Tools ',
   },
 ];
 
