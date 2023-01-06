204b9541a22ecbbf2494fca170c16aa97a98701f
Abraham Ibizugbe <abrahamibizugbe@gmail.com>
2023-01-06 09:49:32 +0100

    adjiust style in skills section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -16,10 +16,14 @@ function Skills() {
       <div className="about">
         <h1 className="about-header">About Myself</h1>
         <h1 className="about-text">
-          Hello I’m a software developer! I can help you build a product ,
-          feature or website Look through some of my work and experience! If you
-          like what you see and have a project you need coded, don’t hestiate to
-          contact me.
+          Full-stack web developer and enthusiastic learner
+          who values accessible design and clean code. An enthusiast
+          for remote work and pair programming. I have great experience
+          in the scrum method, problem-solving, test-driven development,
+          and code reviews while studying at Microverse.  Fluent in ruby,
+          javascript, react, ruby on rails, and other technologies. Confident
+          communicator, strategic thinker, and innovative creator to
+          develop customized software.
         </h1>
         <p className="connect">LET’S CONNECT</p>
         <ul className="connect-links animate__animated animate__bounce">
@@ -55,7 +59,7 @@ function Skills() {
           </button>
         </a>
       </div>
-      <div className="language-container">
+      <div className="language-container lg:pt-10">
         <div className="accordion">
           {accordionData.map(({ title, content }) => (
             <Accordion title={title} content={content} key="3" />
